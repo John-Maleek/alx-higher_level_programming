7d6abf00f103e8d72d74d047f63fe31883537883
Hafeez Abdulmaleek <jmzoran01@gmail.com>
2023-08-16 14:09:53 -0700

    second commit

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     sorted_dic = sorted(a_dictionary)
-    return {item: a_dictionary.get(item) for item in sorted_dic}
+    print({item: a_dictionary.get(item) for item in sorted_dic})
