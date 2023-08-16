9c90aa0d0ac60e8c5f60afe031be0429c310f858
Hafeez Abdulmaleek <jmzoran01@gmail.com>
2023-08-16 15:47:49 -0700

    third commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,7 @@ def best_score(a_dictionary):
     if isinstance(a_dictionary, dict):
         keys_list = list(a_dictionary)
         values_list = [a_dictionary[item] for item in keys_list]
-        return values_list
+        values_list.sort()
+        return values_list[-1]
     else:
         return None
---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    str_list = list(roman_string)
+    num_list = [roman_dict.get(str) for str in str_list]
+    list_len = len(num_list) - 1
+    arr = []
+    sum = 0
+    i = 0
+    while i <= list_len:
+        if list_len == 0:
+            arr.append(num_list[0])
+        elif i == list_len:
+            arr.append(num_list[i])
+        elif num_list[i] > num_list[i + 1] or num_list[i] == num_list[i + 1]:
+            arr.append(num_list[i])
+        else:
+            arr.append(-num_list[i])
+        i += 1
+    for num in arr:
+        sum = sum + num
+    return sum
