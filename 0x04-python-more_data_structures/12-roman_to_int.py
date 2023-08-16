c6430a85a42e66d303fce122f0205985cbe74c6c
Hafeez Abdulmaleek <jmzoran01@gmail.com>
2023-08-16 16:01:47 -0700

    third commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,8 @@
 def roman_to_int(roman_string):
     if isinstance(roman_string, str) is False or roman_string is None:
         return 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+                  'M': 1000}
     str_list = list(roman_string)
     num_list = [roman_dict.get(str) for str in str_list]
     list_len = len(num_list) - 1
