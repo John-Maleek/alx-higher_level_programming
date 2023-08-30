85b21f3b91a840ba05ce5f05dd6c86e70d0658f3
Hafeez Abdulmaleek <jmzoran01@gmail.com>
2023-08-30 15:43:00 -0700

    advanced tasks attempt

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    try:
+        res = fct(*args)
+        return res
+    except (ValueError, ZeroDivisionError, IndexError, TypeError) as error:
+        print("Exception: {}".format(error), file=sys.stderr)
+        return None
