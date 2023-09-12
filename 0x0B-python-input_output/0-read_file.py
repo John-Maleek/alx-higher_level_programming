52e1d113852e370d1e416ca80a70c2274ffb6a31
Hafeez Abdulmaleek <jmzoran01@gmail.com>
2023-09-12 15:41:12 -0700

    first commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Module reads the content of a file"""
+
 
 def read_file(filename=""):
+    """prints the content of a file"""
     with open(filename, mode="r", encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Module writes to a text file"""
+
 
 def write_file(filename="", text=""):
-    with open(filename, mode="r", encoding="utf-8") as file:
+    """writes to a file
+    Return number of characters written
+    """
+    with open(filename, mode="w", encoding="utf-8") as file:
         return file.write("{}\n".format(text))
