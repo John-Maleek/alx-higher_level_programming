1fb49d0e6cb1f72f22aeacf787ccd81ec894cde8
Hafeez Abdulmaleek <jmzoran01@gmail.com>
2023-09-12 16:29:08 -0700

    third commit

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def load_from_json_file(filename):
     """creates an Object from a “JSON file”"""
-    with open(filename, mode="r", encoding+"utf-8") as fd:
+    with open(filename, mode="r", encoding="utf-8") as fd:
         return json.loads(fd)
