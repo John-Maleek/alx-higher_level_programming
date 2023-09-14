1ac4a345065ce72d362f05b17dce25572421f4b6
Hafeez Abdulmaleek <jmzoran01@gmail.com>
2023-09-14 00:59:38 -0700

    new correction

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+'''
+    Implementing a class, Geometry
+'''
 
 
 BaseGeometry = __import__("7-base_geometry.py").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-        defines a subclass of BaseGeometry:w
-    """
+    '''
+        defines a subclass of BaseGeometry
+    '''
     def __init__(self, width, height):
         """initialises Rectangle class"""
         self.integer_validator('width', width)
