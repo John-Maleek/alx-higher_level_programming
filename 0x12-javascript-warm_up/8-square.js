1286b8b276bc5bac0a9cbfe4f8e381b1aea90160
Hafeez Abdulmaleek <jmzoran01@gmail.com>
2023-11-13 15:41:04 -0800

    first commit

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const args = process.argv[2];
+
+if (isNaN(args)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < args; i++) {
+    let line = '';
+    for (let j = 0; j < args; j++) {
+      line = line + 'X';
+    }
+    console.log(line);
+  }
+}
